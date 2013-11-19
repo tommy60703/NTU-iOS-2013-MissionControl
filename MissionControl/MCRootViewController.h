cf627183f72047e8c8d11ca594c6f26c30e78f0b
tommy60703 <tommy60703@gmail.com>
2013-11-19 11:27:48 +0800

    add a button

--- MissionControl/MCRootViewController.h
+++ MissionControl/MCRootViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface MCRootViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *welcomeText;
 
 @end
--- MissionControl/MCRootViewController.m
+++ MissionControl/MCRootViewController.m
@@ -35,4 +35,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)welcomeClicked {
+    self.welcomeText.text = @"Welcome to Xcode & git";
+}
+
 @end
