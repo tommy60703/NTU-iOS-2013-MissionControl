12348d5f90148710fcd01307dc0e38c607a00bad
tommy60703 <tommy60703@gmail.com>
2013-11-29 13:20:27 +0800

    做到一半

--- MissionControl/MCRootViewController.m
+++ MissionControl/MCRootViewController.m
@@ -18,6 +18,50 @@
 
 #pragma mark - Table view data source
 
+- (void)viewDidLoad {
+    NSString *destPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    destPath = [destPath stringByAppendingPathComponent:@"projectContent.plist"];
+    
+    // If the file doesn't exist in the Documents Folder, copy it.
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    if (![fileManager fileExistsAtPath:destPath]) {
+        NSLog(@"NO");
+        NSString *sourcePath = [[NSBundle mainBundle] pathForResource:@"projectContent" ofType:@"plist"];
+        [fileManager copyItemAtPath:sourcePath toPath:destPath error:nil];
+    }
+    [self addToMyPlist];
+}
+
+/* Code to write into file */
+
+- (void)addToMyPlist {
+    // check if file exists
+    NSString *plistPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    plistPath = [plistPath stringByAppendingPathComponent:@"projectContent.plist"];
+    
+    // get data from plist file
+    NSMutableArray *plistArray = [[NSMutableArray alloc]
+                                  initWithContentsOfFile:plistPath];
+    
+    // create dictionary using array data and bookmarkKeysArray keys
+    NSArray *keysArray = [[NSArray alloc] initWithObjects:@"StudentNo", nil];
+    NSArray *valuesArray = [[NSArray alloc] initWithObjects:
+                            [NSString stringWithFormat:@"1234"], nil];
+    
+    NSDictionary *plistDict = [[NSDictionary alloc]
+                              initWithObjects:valuesArray
+                              forKeys:keysArray];
+    
+    [plistArray addObject:plistDict];
+    
+    // write data to  plist file
+    //BOOL isWritten = [plistArray writeToFile:plistPath atomically:YES];
+    [plistArray writeToFile:plistPath atomically:YES];
+}
+
+
+
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
@@ -54,8 +98,8 @@
     if ([segue.identifier isEqualToString:@"ShowProjectContent"]) {
         UITableViewCell *cell = (UITableViewCell *)sender;
         NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
-        MCProjectContentViewController *destinationViewController = segue.destinationViewController;
-        destinationViewController.project = [[MCProjects shareData] projectAtIndexPath:indexPath];
+//        MCProjectContentViewController *destinationViewController = segue.destinationViewController;
+//        destinationViewController.project = [[MCProjects shareData] projectAtIndexPath:indexPath];
     }
 }
 
---
+++ MissionControl/MCTimestampCell.h
@@ -0,0 +1,13 @@
+//
+//  MCTimestampCell.h
+//  MissionControl
+//
+//  Created by Tommy Lin on 2013/11/29.
+//  Copyright (c) 2013年 Tommy Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCTimestampCell : UITableViewCell
+
+@end
